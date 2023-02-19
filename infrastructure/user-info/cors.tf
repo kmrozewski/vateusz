2f6af3c7f487dc134044d7dbb19f3a42d32d2945
kmrozewski <ext.konrad.mrozewski@kesko.fi>
2023-02-19 23:23:54 +0100

    Cors adjustments

--- infrastructure/user-info/cors.tf
+++ infrastructure/user-info/cors.tf
@@ -4,10 +4,6 @@ module "cors" {
 
   api_id          = aws_api_gateway_rest_api.proxy.id
   api_resource_id = aws_api_gateway_resource.proxy.id
-
-  allow_headers = local.allow_headers
-  allow_origin  = local.allow_origin
-  allow_methods = local.allow_methods
 }
 
 
